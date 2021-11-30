1d4fc4f57ad0ea731a5c03918cbb6519e0846c46
Frédéric Glorieux <frederic.glorieux@fictif.org>
2021-12-01 00:51:37 +0100

    premières lib aux normes php 7.4

---
+++ _pars.php
@@ -0,0 +1,10 @@
+<?php
+/** Paramètres locaux de connexions à la base MySQL */
+
+return array(
+  'host' => 'localhost',
+  'port' => '3306',
+  'dbname' => 'medict',
+  'user' => '?????',
+  'pass' => '?????',
+);
