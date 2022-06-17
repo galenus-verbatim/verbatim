64b440c19327d3d0c22c79f6c9ba9a6900426d20
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-06-17 16:24:30 +0200

    Add a nice tool page

---
+++ pages/doc.php
@@ -0,0 +1,110 @@
+<?php
+
+require_once(dirname(__DIR__) . "/Italikos.php");
+
+use Oeuvres\Kit\{I18n,Web};
+
+
+class Data {
+    /** requested cts */
+    public static $cts;
+    /** Doc record from database */
+    public static $doc;
+    /** Editio record from database */
+    public static $editio;
+    /** init param */
+    public static function init() {
+        $cts = Web::par('cts');
+        self::$cts = $cts;
+        if (strpos($cts, '_') === false) { // cover
+            $sql = "SELECT * FROM doc WHERE clavis LIKE ? LIMIT 1";
+            $qDoc = Verbatim::$pdo->prepare($sql);
+            $qDoc->execute(array($cts . '%'));
+        }
+        else { // should be a document
+            $sql = "SELECT * FROM doc WHERE clavis LIKE ? LIMIT 1";
+            $qDoc = Verbatim::$pdo->prepare($sql);
+            $qDoc->execute(array($cts. '%'));
+        }
+        self::$doc = $qDoc->fetch(PDO::FETCH_ASSOC);
+        
+        $edclavis = strtok($cts, '_');
+        $sql = "SELECT * FROM editio WHERE clavis = ? LIMIT 1";
+        $qed = Verbatim::$pdo->prepare($sql);
+        $qed->execute(array($edclavis));
+        self::$editio = $qed->fetch(PDO::FETCH_ASSOC);
+    }
+}
+Data::init();
+
+/**
+ * Called by template to give a contextual <title>
+ */
+function title() {
+    $doc = Data::$doc;
+    $editio = Data::$editio;
+    if (!$doc || !$editio) return null;
+    $title = '';
+    $title .= $editio['auctor'];
+    $title .= '. ' .$editio['titulus'];
+    $num = Verbatim::num($doc);
+    if ($num) $title .= ', ' . $num;
+    $title .= ', ed. ' . $editio['editor'];
+    $title .= Verbatim::scope($doc);
+    $title .= '. urn:cts:greekLit:' . preg_replace('@_@', ':', $doc['clavis']);
+    $title .= ' — ' . Verbatim::name();
+    $title = strip_tags($title);
+    return $title;
+}
+
+/**
+ * Called by template to give main content
+ */
+function main() {
+    $cts = Data::$cts;
+    $doc = Data::$doc;
+    $editio = Data::$editio;
+    if (!$doc) {
+        http_response_code(404);
+        echo I18n::_('doc.notfound', Data::$cts);
+        return;
+    }
+    $q = Web::par('q');
+    $clavis = $doc['clavis'];
+
+    // Get existing formids (int) from a free query
+    $forms = array();
+    if ($q) {
+        $field = Web::par('f', 'lem', '/lem|orth/');
+        $forms = Verbatim::forms($q, $field);
+    }
+    $formids = array_keys($forms); // array of ints
+// html
+?>
+<div class="reader">
+    <div class="toc">
+        <?= Verbatim::nav($editio, $doc, $formids) ?>
+    </div>
+    <div class="doc">
+        <main>
+            <header class="doc">
+                <?= Verbatim::ante($doc) ?>
+                <div>
+                    <?= Verbatim::bibl($editio, $doc) ?>
+                </div>
+                <?= Verbatim::post($doc) ?>
+            </header>
+            <div class="doc">
+                <?= Verbatim::hidoc($doc, $formids) ?>
+            </div>
+            <footer class="doc">
+                <?= Verbatim::ante($doc) ?>
+                <?= Verbatim::post($doc) ?>
+            </footer>
+        </main>
+    </div>
+</div>
+<?php // close function
+}
+?>
+
